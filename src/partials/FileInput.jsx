63fca01f608b269cc1e4354638ee3bf10e8a1c8b
SaktiWidyatmaja <saktiwidyatmaja1@gmail.com>
2023-04-11 20:08:36 +0700

    Tambah radio button

--- src/partials/FileInput.jsx
+++ src/partials/FileInput.jsx
@@ -7,6 +7,7 @@ import { searchPathAstar } from "./Astar";
 function FileInput() {
   const [fileContent, setFileContent] = useState("");
   const [data, setData] = useState(null);
+  const [searchMethod, setSearchMethod] = useState("UCS"); // default search method is UCS
 
   const handleFileChange = async (e) => {
     const file = e.target.files[0];
@@ -31,36 +32,94 @@ function FileInput() {
     reader.readAsText(file);
   };
 
-  // const handleButtonClick = () => {
-  //   // Menggunakan isi file yang disimpan di state
-  //   console.log(fileContent);
-  // };
-  const styles = `
-  input[type="file"] {
-    color: white;
-    background-color: transparent;
-    border: none;
-  }
-  `;
+  const handleSearchMethodChange = (e) => {
+    setSearchMethod(e.target.value);
+  };
 
+  const handleSearchButtonClick = async () => {
+    if (!data) return;
 
+    let path;
+    if (searchMethod === "UCS") {
+      path = await searchPathUCS(
+        data.nodes[12],
+        data.nodes[3],
+        data.nodes,
+        data.weightedAdjacencyMatrix
+      );
+      console.log("UCS Result");
+      console.log(path);
+    } else {
+      path = await searchPathAstar(
+        data.nodes[12],
+        data.nodes[3],
+        data.nodes,
+        data.weightedAdjacencyMatrix
+      );
+      console.log("AStar Result");
+      console.log(path);
+    }
+  };
 
   return (
     <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
       <div data-aos="fade-up" data-aos-delay="400" data-aos-offset="1">
         {/* <MapComponent data = {data} /> */}
-        {data ? <MapComponent data={data} /> : null}
+        <div>
+          {data ? <MapComponent data={data} /> : null}
+        </div>
 
-        {/* <style>{styles}</style> */}
-        <input className="
-        btn-sm 
-        text-white 
-        bg-purple-600 
-        hover:bg-purple-700 
-        ml-3 
-        z-10" 
-        type="file" accept=".txt" onChange={handleFileChange} />
+        <div>
+          <input className="
+            btn-sm 
+            text-white 
+            bg-purple-600 
+            hover:bg-purple-700 
+            ml-3 
+            z-10" 
+            type="file" accept=".txt" onChange={handleFileChange} />
+        </div>
+
+        <div className="mt-3 flex items-center justify-center">
+          <label className="mr-2">Search Method:</label>
+          <div className="flex items-center">
+            <div className="mr-2">
+              <input
+                type="radio"
+                id="UCS"
+                name="searchMethod"
+                value="UCS"
+                checked={searchMethod === "UCS"}
+                onChange={handleSearchMethodChange}
+              />
+              <label htmlFor="UCS">UCS</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="AStar"
+                name="searchMethod"
+                value="AStar"
+                checked={searchMethod === "AStar"}
+                onChange={handleSearchMethodChange}
+              />
+              <label htmlFor="AStar">A*</label>
+            </div>
+          </div>
+        </div>
+
+        <button
+          className="
+            btn-sm 
+            text-white 
+            bg-purple-600 
+            hover:bg-purple-700 
+            mt-3 
+            z-10"
+          onClick={handleSearchButtonClick}
+        />
       </div>
+
     </div>
   );
 }
